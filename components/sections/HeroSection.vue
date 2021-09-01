7f136b945ef4f1f8ce08b02dbb69a25e12b4b6e2
Maxim Belkin <belkinurban@gmail.com>
2021-09-01 19:07:26 +0200

    feat: :sparkles: Hero section basic layout

---
+++ components/sections/HeroSection.vue
@@ -0,0 +1,45 @@
+<template>
+  <section class="section-hero">
+    <div class="hero">
+      <div class="hero-textbox">
+        <h1 class="heading-primary">
+          A healthy meal delivered to your door, every single day
+        </h1>
+        <p class="hero-description">
+          Hello, we’re Omnifood, your new premium food delivery service. We know
+          you’re always busy. No time for cooking. So let us take care of that,
+          we’re really good at it, we promise!
+        </p>
+        <a href="#" class="cta-main-btn">Start eating well</a>
+        <a href="#" class="cta-secondary-btn">Learn more &darr;</a>
+      </div>
+      <div class="hero-imgbox">
+        <img class="hero-thumb" src="/resources/hero.jpg" alt="Food delivery" />
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.section-hero {
+  .hero {
+    max-width: 130rem;
+    margin: 0 auto;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+
+    &-textbox {
+      .heading-primary {
+      }
+      .hero-description {
+      }
+    }
+
+    &-imgbox {
+      .hero-thumb {
+        width: 100%;
+      }
+    }
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,52 @@
 <template>
-  <div>
-    <Nuxt />
-  </div>
+  <section>
+    <HeroSection />
+    <main>
+      <Nuxt />
+    </main>
+  </section>
 </template>
+
+<script>
+import HeroSection from "@/components/sections/HeroSection.vue";
+export default {
+  components: {
+    HeroSection
+  }
+};
+</script>
+
+<style lang="scss">
+/* ---------------------------------- Basic --------------------------------- */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  font-size: 62.5%;
+}
+
+html,
+body {
+  background-color: #fff;
+  color: #555;
+  font-family: "Lato", "Arial", sans-serif;
+  font-weight: 400;
+  line-height: 1;
+  text-rendering: optimizeLegibility;
+  overflow-x: hidden;
+}
+
+.clearfix {
+  zoom: 1;
+}
+.clearfix:after {
+  content: ".";
+  clear: both;
+  display: block;
+  height: 0;
+  visibility: hidden;
+}
+</style>
